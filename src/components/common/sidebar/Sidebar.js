c42e8a5692dcbf92c41626869635c74e5228d174
Anarion <anarion80@gmail.com>
2022-12-20 14:51:05 +0100

    Translate Sidebar title

--- src/components/common/sidebar/Sidebar.js
+++ src/components/common/sidebar/Sidebar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import SidebarPostCard from './SidebarPostCard'
 import { useStaticQuery, graphql } from 'gatsby'
+import { useLang, getTranslation } from '../../../utils/use-lang'
 
 const Sidebar = () => {
+    const t = getTranslation(useLang())
     const featuredPosts = useStaticQuery(graphql`
         query GhostFeaturedPostsQuery {
         allGhostPost(
@@ -23,7 +25,7 @@ const Sidebar = () => {
     //const featuredPosts = posts.filter(item => item.node.featured)
         <aside className="sidebar col s12 l4 flex flex-col justify-start mt-12 lg:mt-0">
             <div className="flex flex-col flex-1">
-                <SidebarPostCard widgetTitle="Featured Articles" widgetPosts={featuredPosts} />
+                <SidebarPostCard widgetTitle={t(`Featured_Articles`)} widgetPosts={featuredPosts} />
             </div>
         </aside>
     )
