38c22369ba33c2825bc45e24c6a45706a57443a3
Anarion <anarion80@gmail.com>
2022-12-20 15:37:21 +0100

    Add missing gatsby plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -195,6 +195,17 @@ module.exports = {
                 addUncaughtPages: true,
             },
         },
+        {
+            resolve: `gatsby-plugin-netlify`,
+            options: {
+                //headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+                //allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+                //mergeSecurityHeaders: true, // boolean to turn off the default security headers
+                mergeCachingHeaders: false, // boolean to turn off the default caching headers
+                //transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+                generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+            },
+        },
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-offline`,
         //`gatsby-plugin-dark-mode`,
